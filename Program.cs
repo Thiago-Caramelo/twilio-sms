d803fde313472b97ae709033e8319c6760c60b5c
Thiago Caramelo <thiago.pescado@gmail.com>
2020-03-29 13:56:52 -0300

    Send SMS

--- Program.cs
+++ Program.cs
@@ -1,12 +1,36 @@
 ﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
 
-namespace twilio_sms
+
+class Program
 {
-    class Program
+  static void Main(string[] args)
+  {
+    switch (args[0])
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello World!");
-        }
+      case "send":
+        SendSms();
+        break;
+      case "receive":
+        break;
+      default:
+        throw new ArgumentException("Invalid Argument Value");
     }
+  }
+  private static void SendSms()
+  {
+    string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+    string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+    TwilioClient.Init(accountSid, authToken);
+
+    var message = MessageResource.Create(
+        body: "You have a New Order #456",
+        from: new Twilio.Types.PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_FROM")),
+        to: new Twilio.Types.PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_TO"))
+    );
+
+    Console.WriteLine(message.Sid);
+  }
 }

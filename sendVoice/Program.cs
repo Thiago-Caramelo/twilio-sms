694a0b860562aaba3e570cce8f4436454448c74d
Thiago Caramelo <thiago.pescado@gmail.com>
2020-03-30 14:35:29 -0300

    Send a Messsage Voice

---
+++ sendVoice/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Twilio;
+using Twilio.Rest.Api.V2010.Account;
+using Twilio.Types;
+
+namespace sendVoice
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string accountSid = Environment.GetEnvironmentVariable("TWILIO_ACCOUNT_SID");
+      string authToken = Environment.GetEnvironmentVariable("TWILIO_AUTH_TOKEN");
+
+      TwilioClient.Init(accountSid, authToken);
+
+      var to = new PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_TO"));
+      var from = new PhoneNumber(Environment.GetEnvironmentVariable("TWILIO_FROM"));
+      var call = CallResource.Create(to, from,
+          url: new Uri("http://demo.twilio.com/docs/voice.xml"));
+
+      Console.WriteLine(call.Sid);
+    }
+  }
+}

518446ce3586fb4c8959fc10ae82e38eba5eaef0
Thiago Caramelo <thiago.pescado@gmail.com>
2020-03-31 09:40:15 -0300

    Receive Controller

---
+++ receiveVoice/Controllers/VoiceController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using receiveVoice.Models;
+using Twilio.AspNet.Core;
+using Twilio.TwiML;
+
+namespace receiveVoice.Controllers
+{
+  public class VoiceController : TwilioController
+  {
+    [HttpPost]
+    public IActionResult Index()
+    {
+      var response = new VoiceResponse();
+      response.Say("How are you ? Have a great day.");
+
+      return TwiML(response);
+    }
+  }
+}
